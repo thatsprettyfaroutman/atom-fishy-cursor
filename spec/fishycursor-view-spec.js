647ed97dcd21df471297588ee30f51222353abfb
Drone CI <ci@hoxhunt.com>
2019-08-22 11:37:16 +0300

    Fishy cursor wip

---
+++ spec/fishycursor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FishycursorView from '../lib/fishycursor-view';
+
+describe('FishycursorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
